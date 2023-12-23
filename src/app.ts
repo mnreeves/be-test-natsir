9b7161f4bc38ebb257bf17b5de1e4436543ba314
mnreeves <natsir.productions@gmail.com>
2023-12-23 10:03:14 +0700

    setup app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import Express, { Application } from "express";
+import Cors from "cors";
+import Dotenv from "dotenv";
+
+Dotenv.config();
+
+const PORT = process.env.PORT;
+const app: Application = Express();
+
+app.use(Cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: false }));
+app.use("/", async (req, res) => {
+  res.status(200).send({ message: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
+});
