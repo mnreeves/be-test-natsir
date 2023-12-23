b62f6e7570f935639ce6775095559b58512918dd
mnreeves <natsir.productions@gmail.com>
2023-12-23 10:47:25 +0700

    feat: create user body middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Express, { Application, NextFunction, Request, Response } from "express";
 import Cors from "cors";
 import Dotenv from "dotenv";
+import { body, validationResult } from "express-validator";
 
 Dotenv.config();
 
@@ -26,12 +27,44 @@ const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-app.post("/v1/user/create", apiKeyMiddleware, async (req, res) => {
-  res.status(200).send({
-    message: "okay",
-    result: req.body,
-  });
-});
+const validateCreateUserBodyMiddleware = [
+  body("username")
+    .notEmpty()
+    .withMessage("username is required")
+    .isLength({ min: 5 })
+    .withMessage("username must be at least 5 characters long"),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // could be changed the shape of response later,
+      // but for now only need to take the error message
+      const statusDescription =
+        errors.array().length > 0
+          ? errors.array()[0].msg ?? "bad request"
+          : "bad request";
+      return res.status(400).json({
+        statusCode: 400,
+        statusMessage: "bad request",
+        statusDescription,
+      });
+    }
+
+    next();
+  },
+];
+
+app.post(
+  "/v1/user/create",
+  apiKeyMiddleware,
+  validateCreateUserBodyMiddleware,
+  async (req: Request, res: Response) => {
+    res.status(200).send({
+      message: "okay",
+      result: req.body,
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
